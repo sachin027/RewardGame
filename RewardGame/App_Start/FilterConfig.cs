b23e907cfa7cf5291aee323aa11f629b3566f48a
Sachin Kanzariya <sachin.k@shaligraminfotech.com>
2024-06-08 17:37:03 +0530

    API

--- RewardGame/App_Start/FilterConfig.cs
+++ RewardGame/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Mvc;
 
 namespace RewardGame
@@ -7,7 +8,7 @@ namespace RewardGame
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new HandleErrorAttribute() { ExceptionType = typeof(Exception), View = "Error" });
         }
     }
 }
