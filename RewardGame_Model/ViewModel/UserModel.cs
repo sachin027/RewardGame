8d706bd57aaaf132e7595dbfdc0b84e12876d8e7
Sachin Kanzariya <sachin.k@shaligraminfotech.com>
2024-06-08 17:27:11 +0530

    last change

--- RewardGame_Model/ViewModel/UserModel.cs
+++ RewardGame_Model/ViewModel/UserModel.cs
@@ -13,19 +13,18 @@ namespace RewardGame_Model.ViewModel
         public int UserID { get; set; }
 
         [Required(ErrorMessage = "Please enter Username")]
-        //[StringLength(10,ErrorMessage ="Username length should be max 10 characters.")]
-        //[RegularExpression(@"^[a-zA-Z0-9_.]{8,20}$", ErrorMessage = " Username can only contain alphanumeric characters, numbers, underscore (_) and dot (.) ")]
+        [StringLength(50,ErrorMessage ="Username length should be max 50 characters.")]
         public string Username { get; set; }
 
         [Required(ErrorMessage = "Please enter Email")]
-        //[RegularExpression(@"^\S+@\S+\.\S+$" , ErrorMessage ="Please enter valid Email Id")]
-        //[MaxLength(50,ErrorMessage ="Email id length should be 50 characters")]
+        [RegularExpression(@"^\S+@\S+\.\S+$" , ErrorMessage ="Please enter valid Email Id")]
+        [MaxLength(50,ErrorMessage ="Email id length should be 50 characters")]
         public string EmailId { get; set; }
 
         [Required(ErrorMessage = "Please enter Password")]
         //[RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", ErrorMessage ="password must contain at least one numbers , one capital letter , one small letter and one special character.")]
-        //[MaxLength(30, ErrorMessage ="password maximum length should be 30 characters.")]
-        //[MinLength(8, ErrorMessage ="password minimum length should be 8 characters.")]
+        [MaxLength(30, ErrorMessage ="password maximum length should be 30 characters.")]
+        [MinLength(8, ErrorMessage ="password minimum length should be 8 characters.")]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "Please enter Confirm Password")]
